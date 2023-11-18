fcc2a74573d100ed101501d670d2ac02bf78a810
David Baker <dpbaker1298@gmail.com>
2023-11-17 20:17:43 -0500

    Refactor interval struct and configuration

--- pomodoro/interval.go
+++ pomodoro/interval.go
@@ -1,17 +1,15 @@
 package pomodoro
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"time"
 )
 
 // Category constants
 const (
-	CategoryPomodoro = "Pomodoro"
+	CategoryPomodoro   = "Pomodoro"
 	CategoryShortBreak = "ShortBreak"
-	CategoryLongBreak = "LongBreak"
+	CategoryLongBreak  = "LongBreak"
 )
 
 // State constants
@@ -25,12 +23,12 @@ const (
 
 // Struct to represent a Pomodoro interval
 type Interval struct {
-	ID int64
-	StartTime time.Time
+	ID              int64
+	StartTime       time.Time
 	PlannedDuration time.Duration
-	ActualDuration time.Duration
-	Category string
-	State int
+	ActualDuration  time.Duration
+	Category        string
+	State           int
 }
 
 // "Repository Patter" - Abstract the data source by defining the interface here
@@ -40,33 +38,33 @@ type Repository interface {
 	Update(i Interval) error
 	ByID(id int64) (Interval, error)
 	Last() (Interval, error)
-	Breaks(n int) ([]Interval, error) 
+	Breaks(n int) ([]Interval, error)
 }
 
 // Error Values that this may return
 var (
-	ErrNoIntervals = errors.New("No intervals")
+	ErrNoIntervals        = errors.New("No intervals")
 	ErrIntervalNotRunning = errors.New("Interval not running")
-	ErrIntervalCompleted = errors.New("Interval is completed or cancelled")
-	ErrInvalidState = errors.New("Invalid State")
-	ErrInvalidID = errors.New("Invalid ID")
+	ErrIntervalCompleted  = errors.New("Interval is completed or cancelled")
+	ErrInvalidState       = errors.New("Invalid State")
+	ErrInvalidID          = errors.New("Invalid ID")
 )
 
 // Configuration required to instantiate an interval
 type IntervalConfig struct {
-	repo Repository
-	PomodoroDuration time.Duration
+	repo               Repository
+	PomodoroDuration   time.Duration
 	ShortBreakDuration time.Duration
-	LongBreakDuration time.Duration
+	LongBreakDuration  time.Duration
 }
 
 // This function uses the values provided by the user or sets defaults for each interval
 func NewConfig(repo Repository, pomodoro, shortBreak, longBreak time.Duration) *IntervalConfig {
 	c := &IntervalConfig{
-		repo: repo,
-		PomodoroDuration: 25 * time.Minute,
+		repo:               repo,
+		PomodoroDuration:   25 * time.Minute,
 		ShortBreakDuration: 5 * time.Minute,
-		LongBreakDuration: 15 * time.Minute,
+		LongBreakDuration:  15 * time.Minute,
 	}
 
 	if pomodoro > 0 {
@@ -83,3 +81,4 @@ func NewConfig(repo Repository, pomodoro, shortBreak, longBreak time.Duration) *
 
 	return c
 }
+
