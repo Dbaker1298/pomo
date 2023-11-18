d59bbbc660ef5cf2d7fb9be0b86ee780b2debb56
David Baker <dpbaker1298@gmail.com>
2023-11-18 13:41:57 -0500

    Set up Cobra root cmd file and commands

--- cmd/root.go
+++ cmd/root.go
@@ -17,26 +17,49 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"fmt"
+	"io"
 	"os"
+	"time"
 
+	"github.com/Dbaker1298/pomo/app"
+	"github.com/Dbaker1298/pomo/pomodoro"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
-
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pomo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Interactive Pomodoro Timer",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		config := pomodoro.NewConfig(
+			repo,
+			viper.GetDuration("pomo"),
+			viper.GetDuration("short"),
+			viper.GetDuration("long"),
+		)
+
+		return rootAction(os.Stdout, config)
+	},
+}
+
+func rootAction(out io.Writer, config *pomodoro.IntervalConfig) error {
+	a, err := app.New(config)
+	if err != nil {
+		return err
+	}
+
+	return a.Run()
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -48,16 +71,41 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomo.yaml)")
+		// Search config in home directory with name ".pomo" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pomo")
+	}
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
 
+func init() {
+	cobra.OnInitialize(initConfig)
 
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomo.yaml)")
+
+	rootCmd.PersistentFlags().DurationP("pomo", "p", 25*time.Minute, "Duration of a pomodoro")
+	rootCmd.PersistentFlags().DurationP("short", "s", 5*time.Minute, "Duration of a short break")
+	rootCmd.PersistentFlags().DurationP("long", "l", 15*time.Minute, "Duration of a long break")
+
+	viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+	viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+	viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
+}
